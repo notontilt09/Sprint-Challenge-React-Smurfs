ec19ea8c42ccb66c1d858ba409b4e1eb1bc8d233
Daniel Weinman <namniewd@gmail.com>
2019-01-25 11:25:50 -0500

    added a route for each component

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import { Route, Link, NavLink } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -44,8 +45,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm smurfs={this.state.smurfs} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path='/' render={props => <Smurfs {...props} smurfs={this.state.smurfs} /> } />
+        <Route path='/smurf-form' render={props => <SmurfForm {...props} smurfs={this.state.smurfs} /> } />
+        {/* <SmurfForm smurfs={this.state.smurfs} /> */}
+        {/* <Smurfs smurfs={this.state.smurfs} /> */}
       </div>
     );
   }
